9e03357d58d5e028d9b4d6e6a2a52056bec6a4b8
Artem Mironchik <tawerka.power@gmail.com>
2022-11-07 23:58:08 +0300

    fix: implement async await functions to avoid callback hell and delete unnecessary files

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 const getPath = (filename) => path.join(__dirname, filename);
 
-// читаем файлы из директории
-fs.readdir(getPath('secret-folder'), {withFileTypes: true}, (err, files) => {
-    if(err) throw err
+// добавляем async await, чтобы избавиться от callback hell
+async function filesInFolder(folderPath) {
+    // читаем файлы из директории
+    const files = await fsPromises.readdir(getPath(folderPath), {withFileTypes: true})
 
-    else {
-        // пробегаемся по всем файлам
-        files.forEach((file) => {
-            // проверка на то, является ли file файлом
-            if(file.isFile()) {
-                // информация о файле и ее вывод
-                fs.stat(path.join(getPath('secret-folder'), file.name), (err, stats) => {
-                    console.log(`${path.parse(file.name).name} - ${path.parse(file.name).ext.slice(1)} - ${stats.size / 1024}kb`)
-                })
-            }
-        })
+    for(let file of files) {
+        // проверка на то, является ли file файлом
+        if(file.isFile()) {
+            // информация о файле и ее вывод
+            const fileStats = await fsPromises.stat(path.join(getPath(folderPath), file.name))
+            console.log(`${path.parse(file.name).name} - ${path.parse(file.name).ext.slice(1)} - ${fileStats.size / 1024}kb`)
+        }
     }
-})
+}
+
+filesInFolder('secret-folder')
+
