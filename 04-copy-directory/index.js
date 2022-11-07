6e4a3cbc923b4dcb73bf44252ee383633ba0189d
Artem Mironchik <tawerka.power@gmail.com>
2022-11-07 23:49:54 +0300

    feat: complete fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+const getPath = (filename) => path.join(__dirname, filename);
+
+// добавляем async await, чтобы избавиться от callback hell
+async function copyDir(newDirPath, cpyDirPath) {
+    // очищаем всю директорию рекурсией 
+    await fsPromises.rm(newDirPath, {recursive: true, force: true})
+
+    // создаем нашу копию директории
+    await fsPromises.mkdir(newDirPath, {recursive: true})
+
+    // считываем все файлы с директории files
+    const files = await fsPromises.readdir(cpyDirPath)
+    // циклом проходим по всем файлам и копируем их в copy-files
+    for(let file of files) {
+        await fsPromises.copyFile(path.join(cpyDirPath, file), path.join(newDirPath, file))
+    }
+}
+copyDir(getPath('files-copy'), getPath('files'))
